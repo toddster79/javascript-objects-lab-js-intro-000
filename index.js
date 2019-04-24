6c9fd616db296a557850889dcaf0b08fc5bde592
Todd Carlson <todd.carlson3030@me.com>
2019-04-24 04:12:03 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object 
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  Object.assign({}, object, [key]: value);
+  return object 
+}
